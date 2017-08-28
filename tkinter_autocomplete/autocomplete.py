35653454a9f858c41d0f88aca0465ddf6e9a4947
Daniel <Coal0@users.noreply.github.com>
2017-08-28 08:20:32 +0200

    General improvements
    * Replaced `map` with a list comprehension
    * Improving Python 2 LCS performance with `xrange`

--- tkinter_autocomplete/autocomplete.py
+++ tkinter_autocomplete/autocomplete.py
@@ -15,15 +15,28 @@ def _longest_common_substring(s1, s2):
     """Get the longest common substring for two strings.
     Source: https://goo.gl/k64S4f
     """
-    m = [[0] * (1 + len(s2)) for i in range(1 + len(s1))]
+    try:
+        m = [[0] * (1 + len(s2)) for _ in xrange(1 + len(s1))]
+    except NameError:
+        # Python 3
+        m = [[0] * (1 + len(s2)) for _ in range(1 + len(s1))]
     longest, x_longest = 0, 0
-    for x in range(1, 1 + len(s1)):
-        for y in range(1, 1 + len(s2)):
-            if s1[x - 1] == s2[y - 1]:
-                m[x][y] = m[x - 1][y - 1] + 1
-                if m[x][y] > longest:
-                    longest = m[x][y]
-                    x_longest = x
+    try:
+        for x in xrange(1, 1 + len(s1)):
+            for y in xrange(1, 1 + len(s2)):
+                if s1[x - 1] == s2[y - 1]:
+                    m[x][y] = m[x - 1][y - 1] + 1
+                    if m[x][y] > longest:
+                        longest = m[x][y]
+                        x_longest = x
+    except NameError:
+        for x in range(1, 1 + len(s1)):
+            for y in range(1, 1 + len(s2)):
+                if s1[x - 1] == s2[y - 1]:
+                    m[x][y] = m[x - 1][y - 1] + 1
+                    if m[x][y] > longest:
+                        longest = m[x][y]
+                        x_longest = x
             else:
                 m[x][y] = 0
     return s1[x_longest - longest: x_longest]
@@ -169,9 +182,7 @@ class Autocomplete(tk.Frame, object):
         None
         """
         if not case_sensitive:
-            entries = list(map(
-                lambda entry: entry.lower(), entries
-            ))
+            entries = [entry.lower() for entry in entries]
 
         self._case_sensitive = case_sensitive
         self._entries = entries
