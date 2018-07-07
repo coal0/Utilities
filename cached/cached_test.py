087c83f4c6d2abc2eb425403c8a75deb07b6fb30
Daniel <daniel_x3@protonmail.com>
2018-07-07 11:18:41 +0200

    Update cached_test.py

--- cached/cached_test.py
+++ cached/cached_test.py
@@ -2,9 +2,23 @@ from cached import cached
 
 
 class Double:
-    def __init__(self, x):
+    def __init__(self, x, counter):
         self._x = x
+        self._counter = counter
     
     @cached("_double_x")
     def value(self):
+        self._counter += 1
         return self._x * 2
+
+if __name__ == "__main__":
+    c = 0
+    d = Double(6, c)
+    
+    val1 = d.value()
+    assert val1 == 12
+    assert c == 1
+    
+    val2 = d.value()
+    assert val2 == 12
+    assert c == 1
