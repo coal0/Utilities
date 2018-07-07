768d9f6b2a199ff2b7fae294a0190d3b1a49f734
Daniel <daniel_x3@protonmail.com>
2018-07-07 11:37:12 +0200

    Update cached_test.py

--- cached/cached_test.py
+++ cached/cached_test.py
@@ -1,24 +1,50 @@
-from cached import cached
+import math
+
+from cached import cached, auto_cached
 
 
-class Double:
-    def __init__(self, x, counter):
+class Number:
+    def __init__(self, x, counters):
         self._x = x
-        self._counter = counter
-    
-    @cached("_double_x")
+        self._counters = counters
+
     def value(self):
-        self._counter += 1
-        return self._x * 2
+        return self._x
+
+    @cached("_sqrd_x")
+    def value_squared(self):
+        # Keep track of the amount of times this method has been called
+        self._counters["value_squared"] += 1
+        return self._x ** 2
+
+    @cached("_sqrt_x")
+    def square_root_value(self):
+        # Keep track of the amount of times this method has been called
+        self._counters["square_root_value"] += 1
+        return math.sqrt(self._x)
+
 
 if __name__ == "__main__":
-    c = 0
-    d = Double(6, c)
-    
-    val1 = d.value()
-    assert val1 == 12
-    assert c == 1
-    
-    val2 = d.value()
-    assert val2 == 12
-    assert c == 1
+    x = 25
+    counters = {"value_squared": 0, "square_root_value": 0}
+    number = Number(x=x, counters=counters)
+
+    assert number.value() == x
+
+    assert number.value_squared() == x ** 2
+    assert counters["value_squared"] == 1
+
+    assert number.square_root_value() == math.sqrt(x)
+    assert counters["square_root_value"] == 1
+
+    # Show that the result is now cached
+    number.value_squared()
+    number.value_squared()
+    assert counters["value_squared"] == 1
+
+    # Show that the result is now cached
+    number.square_root_value()
+    number.square_root_value()
+    assert counters["square_root_value"] == 1
+
+    print("All tests passed successfully.")
