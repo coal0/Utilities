301fd23faf433bf42c45d8e9325f2e8adbd932db
Daniel <daniel_x3@protonmail.com>
2018-07-07 11:40:22 +0200

    Update cached_test.py

--- cached/cached_test.py
+++ cached/cached_test.py
@@ -23,10 +23,16 @@ class Number:
         self._counters["square_root_value"] += 1
         return math.sqrt(self._x)
 
+    @auto_cached
+    def value_cubed(self):
+        # Keep track of the amount of times this method has been called
+        self._counters["value_cubed"] += 1
+        return self._x ** 3
+
 
 if __name__ == "__main__":
     x = 25
-    counters = {"value_squared": 0, "square_root_value": 0}
+    counters = {"value_squared": 0, "square_root_value": 0, "value_cubed": 0}
     number = Number(x=x, counters=counters)
 
     assert number.value() == x
@@ -37,14 +43,22 @@ if __name__ == "__main__":
     assert number.square_root_value() == math.sqrt(x)
     assert counters["square_root_value"] == 1
 
-    # Show that the result is now cached
+    assert number.value_cubed() == x ** 3
+    assert counters["value_cubed"] == 1
+
+    # Show that x ^ 2 is now cached
     number.value_squared()
     number.value_squared()
     assert counters["value_squared"] == 1
 
-    # Show that the result is now cached
+    # Show that sqrt(x) is now cached
     number.square_root_value()
     number.square_root_value()
     assert counters["square_root_value"] == 1
+
+    # Show that x ^ 3 is now cached
+    number.value_cubed()
+    number.value_cubed()
+    assert counters["value_cubed"] == 1
 
     print("All tests passed successfully.")
