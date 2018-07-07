cf11903f15d25ee3a6028046b9405b11cc786acd
Daniel <daniel_x3@protonmail.com>
2018-07-07 11:29:56 +0200

    Update cached.py

--- cached/cached.py
+++ cached/cached.py
@@ -1,5 +1,8 @@
+import functools
+
 def cached(name):
     def wrapper(function):
+        @functools.wraps(function)
         def call(self, *args, **kwargs):
             if not hasattr(self, name):
                 return_value = function(self, *args, **kwargs)
@@ -7,3 +10,14 @@ def cached(name):
             return getattr(self, name)
         return call
     return wrapper
+
+
+def cached_auto(function):
+    @functools.wraps(function)
+    def call(self, *args, **kwargs):
+        name = "__cached::{}".format(function.__name__)
+        if not hasattr(self, name):
+            return_value = function(self, *args, **kwargs)
+            setattr(self, name, return_value)
+        return getattr(self, name)
+    return call
